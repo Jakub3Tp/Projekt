213d13777ae8c424bc3071cbafb2ce1b95c17ef8
kubix <jakub.gola2@teb.edu.pl>
2025-03-10 22:06:53 +0100

    10.03.2025-layout

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,7 @@
 export default function Home() {
     return <>
         <div className="container d-flex justify-content-center text-center">
-            <h1>Witamy w servisie rejestrowań spotkań</h1>
+            <h1>Witamy w serwisie rejestrowań spotkań</h1>
 
         </div>
     </>
