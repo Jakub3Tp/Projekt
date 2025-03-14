5b08f79644912141890eaa05d8bb09aeb97cfe5b
Name <mymail@example.com>
2025-03-14 13:42:45 +0100

    14.03.2025 - Jakub

--- src/Reservation.jsx
+++ src/Reservation.jsx
@@ -2,7 +2,7 @@ export default function Home() {
 
     return <>
         <div className="container d-flex justify-content-center text-center">
-            <h1>Grafik Spotkań</h1>
+            <h1>Grafik twoich zarejesrowanych spotkań</h1>
         </div>
         <p>Tu jesze nic nie ma :P. (ALE BĘDZIE!)</p>
     </>
